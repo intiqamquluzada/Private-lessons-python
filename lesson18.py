d2e07a216fecf4e9fcf93c9c10abf5c3f7e9da9f
intiqamquluzada <quluzadintiqam@gmail.com>
2023-09-09 21:53:04 +0400

    Exceptions

---
+++ lesson18.py
@@ -0,0 +1,24 @@
+# soz = input("sozleri daxil edin:").split()
+# print(soz)
+# # print(soz[0][-1:-3:-1])
+# soyad = []
+# i = 0
+# while i < len(soz):
+#     if (soz[i][-1:-3:-1])[::-1] == "li" or (soz[i][-1:-5:-1])[::-1] == "zade":  # True or
+#         soyad.append(soz[i])
+#     i += 1
+# print(soyad)
+
+# a = 'salam'
+# print(a[::-1])
+#
+# print("".join(list(reversed(a))))
+
+
+# mystr = "SALAM"
+
+
+# print(mystr[0:5:2])
+# print(mystr[-1:-3:-1][::-1])
+
+
---
+++ lesson19.py
@@ -0,0 +1,91 @@
+# a = int(input("Eded daxil edin: "))
+# def myfunc(eded):
+#     eded += 1
+#     return eded
+# print(myfunc(a))
+
+# global deyisen
+# x = 5
+# def myfunc():
+#     return x
+# print(myfunc())
+
+# function scope
+# def myfunc(eded):
+#     x = 5
+#     return eded
+# print(x)
+
+# x = 7
+# def myfunc(eded):
+#     x = 5
+#     global x
+
+
+# myfunc = lambda a : a + "salam"
+# print(myfunc('ders '))
+
+# x = lambda a, b: a * b
+# print(x(5, 6))
+
+
+# def myfunc(eded):
+#     x = lambda a: a * eded
+#     return x
+#
+#
+# myfunc_1_result = myfunc(5)
+# print(myfunc_1_result(3))
+
+# myfunc = lambda a,b : a * b
+# print(myfunc(3,4))
+
+
+# EXCEPTIONS
+
+# a = 0
+# b = 5
+#
+# print(b/a) # ZERODIVISIONERROR
+
+
+# with open("tasks.txt", 'r') as file1:
+#     file1.write("salam")  # FILENOTFOUND ERROR
+
+# for i in range(1,10):
+# print('salam') #INDENTATIONERROR
+
+# if a == 7:
+#     print('salam')
+
+
+# def myfunc(a):
+#     a = a + 10
+#     return a
+#
+#
+# print(myfunc(False))
+
+
+# try:
+#     first_num = int(input("Ededi daxil edin: "))
+#     second_num = int(input("2ci ededi daxil edin"))
+#     print(first_num/second_num)
+# except ValueError:
+#     print("Deyisenlerden biri eded deyil")
+# except ZeroDivisionError:
+#     print("ededlerden biri sifirdir.")
+# else:
+#     print("Error yoxdur")
+# finally:
+#     print("Men finally blokuyam Hemise isleyirem")
+
+
+# a = 9
+# soz = input("soz daxil edin: ")
+# assert len(soz) < 6, "Sozun uzunlugu cox oldu"
+
+
+# mydict = {
+#     "": ""
+# }
