c223b841dfa00688549c8fa9143d90041ee9e6d7
intiqamquluzada <quluzadintiqam@gmail.com>
2023-10-05 17:33:31 +0400

    nn

--- lesson14.py
+++ lesson14.py
@@ -1,121 +1,135 @@
 
-# 2. mydict = {
-#         '1': 'Lesson',
-#         '2': 'Lesson2',
-#         '3': 'Lesson3',
-# }
-# myNewdict = {}
-# Bu dictionary-nin elementlərini dövr operatoru vasitəsilə myNewdict-ə əlavə edən funksiya yazın.
+# # 2. mydict = {
+# #         '1': 'Lesson',
+# #         '2': 'Lesson2',
+# #         '3': 'Lesson3',
+# # }
+# # myNewdict = {}
+# # Bu dictionary-nin elementlərini dövr operatoru vasitəsilə myNewdict-ə əlavə edən funksiya yazın.
 
 
 
-# mydict = {
-#         1: 'Lesson',
-#         2: 'Lesson2',
-#         3: 'Lesson3',
-# }
+# # mydict = {
+# #         1: 'Lesson',
+# #         2: 'Lesson2',
+# #         3: 'Lesson3',
+# # }
 
-# myNewdict = {}
-
-
-# for key in mydict.keys(): #[1,2,3]
-#     for value in mydict.values(): #[Lesson, Lesson2, Lesson3]
-#         myNewdict.update({key:value})
-# print(myNewdict)
-
-# 4. İstifadəçi float tip bir ədəd daxil edəcək.
-# Bu ədddə vergüldən sonrakı hissənin rəqəmlərini bir list-ə əlavə edən funksiya yazın.
+# # myNewdict = {}
 
 
-# eded = float(input("Ededi daxil edin: ")) # 8.55786 -- > '8.55786' --> ['8', '55786']
+# # for key in mydict.keys(): #[1,2,3]
+# #     for value in mydict.values(): #[Lesson, Lesson2, Lesson3]
+# #         myNewdict.update({key:value})
+# # print(myNewdict)
 
-# def myfunc(eded):
-#     newlist = []
-#     str_eded = str(eded).split(".")
-#     second_part = str_eded[1]
-#     for i in second_part:
-#         newlist.append(i)
-#     newlist = list(map(int, newlist))
-#     return newlist
-# print(myfunc(eded))
-# #8.5
+# # 4. İstifadəçi float tip bir ədəd daxil edəcək.
+# # Bu ədddə vergüldən sonrakı hissənin rəqəmlərini bir list-ə əlavə edən funksiya yazın.
 
 
-# 5. Daxil edilən sözdə ə, ı, ü, ş, ç, ğ olduğu zaman onları 
-# ingilis hərfi ilə əvəz edən funksiya yazın.
+# # eded = float(input("Ededi daxil edin: ")) # 8.55786 -- > '8.55786' --> ['8', '55786']
+
+# # def myfunc(eded):
+# #     newlist = []
+# #     str_eded = str(eded).split(".")
+# #     second_part = str_eded[1]
+# #     for i in second_part:
+# #         newlist.append(i)
+# #     newlist = list(map(int, newlist))
+# #     return newlist
+# # print(myfunc(eded))
+# # #8.5
 
 
-# soz = input("Sozu daxil edin: ")
+# # 5. Daxil edilən sözdə ə, ı, ü, ş, ç, ğ olduğu zaman onları 
+# # ingilis hərfi ilə əvəz edən funksiya yazın.
 
-# def myfunc(soz):
-#     for i in range(len(soz)): #salam -- >  for i in soz: 
-#         if soz[i] == 'ə':
-#             soz = soz.replace(soz[i], "a")
+
+# # soz = input("Sozu daxil edin: ")
+
+# # def myfunc(soz):
+# #     for i in range(len(soz)): #salam -- >  for i in soz: 
+# #         if soz[i] == 'ə':
+# #             soz = soz.replace(soz[i], "a")
         
-#         if soz[i] == "ü":
-#             soz = soz.replace(soz[i], "u")
+# #         if soz[i] == "ü":
+# #             soz = soz.replace(soz[i], "u")
            
-#     return soz
+# #     return soz
 
 
-# print(myfunc(soz))
-# soz = input("sozu daxil edin: ") # əleykim
-# mylist = []
-# az_elifba = ['ə','ü', 'ı', 'ğ', 'ö', 'ç', 'ş']
+# # print(myfunc(soz))
+# # soz = input("sozu daxil edin: ") # əleykim
+# # mylist = []
+# # az_elifba = ['ə','ü', 'ı', 'ğ', 'ö', 'ç', 'ş']
 
-# for i in range(97,123):
-#     mylist.append(chr(i))
-# print(mylist)
-# for i in soz:
-#     if i in az_elifba:
-#         if i == 'ə':
-#             soz = soz.replace(i, 'a')
-#         if i == 'ü':
-#             soz = soz.replace(i, 'u')
-#         if i == 'ı':
-#             soz = soz.replace(i, 'i')
-# print(soz)
-
-
-
-# x = 5   # global tip deyisen
-
-# print(x)
+# # for i in range(97,123):
+# #     mylist.append(chr(i))
+# # print(mylist)
+# # for i in soz:
+# #     if i in az_elifba:
+# #         if i == 'ə':
+# #             soz = soz.replace(i, 'a')
+# #         if i == 'ü':
+# #             soz = soz.replace(i, 'u')
+# #         if i == 'ı':
+# #             soz = soz.replace(i, 'i')
+# # print(soz)
 
 
-# def salamla():
-#     y = 5   # local deyisendir.
 
-#     def sagol():
-#         nonlocal y
-#         y = 3
+# # x = 5   # global tip deyisen
+
+# # print(x)
+
+
+# # def salamla():
+# #     y = 5   # local deyisendir.
+
+# #     def sagol():
+# #         nonlocal y
+# #         y = 3
     
-#     sagol()
-#     print(y)
+# #     sagol()
+# #     print(y)
 
-# salamla()
-
-
-
-
-# -- > read, write.
-
-# open file in current directory
-# file1 = open("test.txt")
-
-# file1.close()
-# print("salam\nsagol")
-
-# print(file1.readlines())
-
-# -- r,w,a
-
-with open("test.txt", "r+") as file1:
-    print(file1.readline())
-    file1.write("\nEstoniya")
+# # salamla()
 
 
 
+
+# # -- > read, write.
+
+# # open file in current directory
+# # file1 = open("test.txt")
+
+# # file1.close()
+# # print("salam\nsagol")
+
+# # print(file1.readlines())
+
+# # -- r,w,a
+
+# with open("test.txt", "r+") as file1:
+#     print(file1.readline())
+#     file1.write("\nEstoniya")
+
+
+
+
+
+sentence = input("Enter the sentence: ")
+my_dict = {}
+
+
+for i in sentence:
+    my_dict.update({i:sentence.count(i)})
+
+sorted_dict = {k: v for k, v in sorted(my_dict.items(), key=lambda item: item[1], reverse=True)}
+
+top_5_dict = dict(list(sorted_dict.items())[:5])
+
+print(top_5_dict)
 
 
 
--- lesson26.py
+++ lesson26.py
@@ -1,62 +1,75 @@
-# soz = 'salam'
-# key = 31
-mlist = [' ', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u',
-         'v', 'w', 'x', 'y', 'z']
-
-
-# myHashed = ''
-# for i in soz:
-#     myvalue = mlist.index(i) + key
-#     mymod = myvalue % 26
-#     newletter = mlist[mymod]
-#     myHashed += newletter
-# print(myHashed)
-
-
-class Car:
-    def __init__(self, color, year):
-        self.color = color
-        self.year = year
-
-    def __str__(self):
-        return self.color
-
-
-carclub = Car(color="----", year="----")
-
-
-class SportCars(Car):
-    def __init__(self, year, color, maxspeed):
-        super().__init__(color, year)
-        self.maxspeed = maxspeed
-
-    def Carpower(self):
-        return self.maxspeed
-
-    def __str__(self):
-        return self.maxspeed
-
-
-car1 = SportCars(year="1976", color="red", maxspeed="320")
-
-# class ClassicCars(Car):
-#     def __init__(self, color, year, model, power):
-#         super().__init__(color, year)
-#         self.power = power
-#         self.model = model
+# # soz = 'salam'
+# # key = 31
+# mlist = [' ', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u',
+#          'v', 'w', 'x', 'y', 'z']
 #
-#     def Carpower(self):
-#         return self.power
+#
+# # myHashed = ''
+# # for i in soz:
+# #     myvalue = mlist.index(i) + key
+# #     mymod = myvalue % 26
+# #     newletter = mlist[mymod]
+# #     myHashed += newletter
+# # print(myHashed)
+#
+#
+# class Car:
+#     def __init__(self, color, year):
+#         self.color = color
+#         self.year = year
 #
 #     def __str__(self):
-#         return self.power
+#         return self.color
 #
 #
-# car2 = ClassicCars(color="white", year="1967", model="Sclass", power="340hp")
-# print(car1)
+# carclub = Car(color="----", year="----")
+#
+#
+# class SportCars(Car):
+#     def __init__(self, year, color, maxspeed):
+#         super().__init__(color, year)
+#         self.maxspeed = maxspeed
+#
+#     def Carpower(self):
+#         return self.maxspeed
+#
+#     def __str__(self):
+#         return self.maxspeed
+#
+#
+# car1 = SportCars(year="1976", color="red", maxspeed="320")
+#
+# # class ClassicCars(Car):
+# #     def __init__(self, color, year, model, power):
+# #         super().__init__(color, year)
+# #         self.power = power
+# #         self.model = model
+# #
+# #     def Carpower(self):
+# #         return self.power
+# #
+# #     def __str__(self):
+# #         return self.power
+# #
+# #
+# # car2 = ClassicCars(color="white", year="1967", model="Sclass", power="340hp")
+# # print(car1)
+#
+#
+# # mystr = "kamaz"
+# # newstr = ""
+# # for i in range(len(mystr)):
+# #     newstr += mystr[i]
 
 
-# mystr = "kamaz"
-# newstr = ""
-# for i in range(len(mystr)):
-#     newstr += mystr[i]
+
+
+
+
+word = input("Enter the word: ")
+my_dict = {}
+
+
+for i in word:
+    my_dict.update({i:word.count(i)})
+print(my_dict)
